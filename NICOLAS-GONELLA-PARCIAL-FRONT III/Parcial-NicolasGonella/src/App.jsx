19417b4e2c4b9f4a677b9333f9dcf7659cca579f
NicolasGonella <nico_gonella@hotmail.com>
2023-05-31 20:19:39 -0300

    Subo modificaciones

--- NICOLAS-GONELLA-PARCIAL-FRONT III/Parcial-NicolasGonella/src/App.jsx
+++ NICOLAS-GONELLA-PARCIAL-FRONT III/Parcial-NicolasGonella/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+
+    </div>
   )
 }
 
